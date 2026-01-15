ce0eb0108293b49da754e8f01a35bd69ea2683b8
Daniel <dany151011@gmail.com>
2026-01-15 18:41:43 +0800

    added enviroment effect, ligth and DOF for HD2D style

--- objects/Player/scripts/run.gd
+++ objects/Player/scripts/run.gd
@@ -9,3 +9,7 @@ func enter()->void:
 func update(_delta:float)->void:
 	if body.direction.x == 0 and body.is_on_floor():
 		statemanager.change_state("idle")
+	if body.direction.x < 0:
+		sprite.flip_h = true
+	if body.direction.x > 0:
+		sprite.flip_h = false
